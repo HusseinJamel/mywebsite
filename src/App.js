586dcd0cbea6673ab40a6f9408a96a3b85b3ebab
HusseinJamel <113606039+HusseinJamel@users.noreply.github.com>
2023-07-30 12:46:19 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./Components/NavBar";
+import Header from "./Components/Header";
+import About from "./Components/About";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+    <NavBar />
+    <Header />
+    <About />
+    </>
+  );
+};
+
+export default App;
